c36b420a3271c772b76ac8b9398afd53096c4a47
Jose Silva <69496369+jmpoblano@users.noreply.github.com>
2021-08-23 11:32:38 -0700

    first commit

---
+++ src/db/migrations/createMoviesTheatersTable.js
@@ -0,0 +1,15 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("movies_theaters", (table) => {
+    table.integer("movie_id").references("movies.movie_id")
+    
+ table.integer("theater_id").references("theaters.theater_id")
+    table.boolean("is_showing")
+//     table.timeStamps(false, true)
+    
+  })
+}
+
+exports.down = function(knex) {
+  return knex.schema.dropTable("theaters")
+  
+}
---
+++ src/db/migrations/createReviewsTable.js
@@ -0,0 +1,18 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("reviews", (table) => {
+    table.increments("review_id").primary()
+    table.text("content")
+    table.integer("score")
+    
+  table.integer("critic_id").references("critics.critic_id")
+    
+  table.integer("movie_id").references("movies.movie_id")
+    //table.timeStamps(false, true)
+    
+  })
+}
+
+exports.down = function(knex) {
+  return knex.schema.dropTable("reviews")
+  
+}
